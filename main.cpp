d29eb4c3a4c5b94b5f98c842184cb3fdfe28260b
Alon15 <alon15@gmail.com>
2016-08-22 18:51:29 +0300

    Added main and main_aux

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "main_aux.h"
+#include "SPImageProc.h"
+#include <stdio.h> // printf, stdout, scanf, snprintf
+#include <stdlib.h> // malloc, EXIT_FAILURE, EXIT_SUCCESS
+#include <string.h> // strcmp
+
+int main() {
+
+	return (EXIT_SUCCESS);
+}
---
+++ main_aux.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_AUX_H_
+#define MAIN_AUX_H_
+
+
+
+#endif /* MAIN_AUX_H_ */
